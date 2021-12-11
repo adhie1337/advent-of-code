1c81d6dff2d16816499bde737ea57675899bdc99
adhie1337 <adhie@inf.elte.hu>
2021-12-11 18:24:28 +0100

    Add day 11 part 1 solution

---
+++ AdventOfCode2021.Tests/Days/Day11Tests.cs
@@ -0,0 +1,34 @@
+namespace AdventOfCode2021.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using AdventOfCode2021.Days.Day11;
+
+    [TestClass]
+    public class Day11Tests
+    {
+        public static string[] ExampleInput = new[]
+        {
+            "5483143223",
+            "2745854711",
+            "5264556173",
+            "6141336146",
+            "6357385478",
+            "4167524645",
+            "2176841721",
+            "6882881134",
+            "4846848554",
+            "5283751526",
+        };
+
+        [TestMethod]
+        public void CountFlashes_ForExample_Returns26397()
+        {
+            var input = ExampleInput;
+
+            var result = Part1.CountFlashes(input);
+
+            Assert.AreEqual<ulong>(1656, result);
+        }
+    }
+}
