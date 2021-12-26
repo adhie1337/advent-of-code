6b5286ad4d7bc53967fc4f5e15c4f2ed79bce8d4
adhie1337 <adhie@inf.elte.hu>
2021-12-26 21:45:27 +0100

    Add 2021 day 23 part 1 solution

---
+++ AdventOfCode2021.Tests/Days/Day23/Example.cs
@@ -0,0 +1,14 @@
+namespace AdventOfCode2021.Tests.Days.Day23
+{
+    public static class Example
+    {
+        public static readonly string[] Input = new[]
+        {
+            "#############",
+            "#...........#",
+            "###B#C#B#D###",
+            "  #A#D#C#A#",
+            "  #########",
+        };
+    }
+}
